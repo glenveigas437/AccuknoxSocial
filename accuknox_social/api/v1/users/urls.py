8abd13844cd40e6752f10569a855a5d3644d8194
Glen Veigas <glenveigas437@gmail.com>
2023-08-26 17:44:22 +0530

    Added User Model and Auth for User

--- accuknox_social/api/v1/users/urls.py
+++ accuknox_social/api/v1/users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from .views import SignupAPIView, LoginAPIView, LogoutAPIView
 
 urlpatterns = [
-    
+    path('signup', SignupAPIView.as_view(), name='signup'),
+    path('login', LoginAPIView.as_view(), name='login'),
+    path('logout', LogoutAPIView.as_view(), name='logout')
 ]
