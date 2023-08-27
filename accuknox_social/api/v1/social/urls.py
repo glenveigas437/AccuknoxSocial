62a50c2c5c133b2570d5187bae920223ee2374cf
Glen Veigas <glenveigas437@gmail.com>
2023-08-27 17:46:40 +0530

    Added Search and Listing all Requests and Users

--- accuknox_social/api/v1/social/urls.py
+++ accuknox_social/api/v1/social/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import FriendRequestView, AcceptRejectFriendRequestView, ListFriendsView
+from .views import FriendRequestView, AcceptRejectFriendRequestView, ListFriendsView, PendingFriendRequestListView
 
 urlpatterns = [
     path('add_friend/<str:recipient_id>', FriendRequestView.as_view(), name='send-friend-request'),
     path('response/<str:friend_request_id>', AcceptRejectFriendRequestView.as_view(), name='accept-reject-friend-request'),
-    path('friends', ListFriendsView.as_view(), name='list-friends')
+    path('friends', ListFriendsView.as_view(), name='list-friends'),
+    path('pending-friend-requests', PendingFriendRequestListView.as_view(), name='pending-friend-requests')
 ]
--- accuknox_social/api/v1/users/urls.py
+++ accuknox_social/api/v1/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import SignupAPIView, LoginAPIView, LogoutAPIView
+from .views import SignupAPIView, LoginAPIView, LogoutAPIView, UserListAPIView
 
 urlpatterns = [
     path('signup', SignupAPIView.as_view(), name='signup'),
     path('login', LoginAPIView.as_view(), name='login'),
-    path('logout', LogoutAPIView.as_view(), name='logout')
+    path('logout', LogoutAPIView.as_view(), name='logout'),
+    path('all', UserListAPIView.as_view(), name='all-users')
 ]
